b24ecc418b57846542c283cc39c9644be13b697c
rafi <rafiannas.id@gmail.com>
2023-02-06 17:29:40 +0700

    add auto scroll

--- src/test/java/jqueryui/Draggable.java
+++ src/test/java/jqueryui/Draggable.java
@@ -1,6 +1,7 @@
 package jqueryui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,7 +25,24 @@ public class Draggable {
 		
 		WebElement drag = driver.findElement(By.id("draggable"));
 		action.clickAndHold(drag).moveByOffset(50, 50).build().perform();
-
+		
+		
+//		auto scroll
+		driver.switchTo().defaultContent();
+		driver.findElement(By.xpath("//a[contains(text(),'Auto-scroll')]")).click();
+		Thread.sleep(5000);
+		
+		driver.switchTo().frame(0);
+		WebElement drag1 = driver.findElement(By.id("draggable"));
+		WebElement drag2 = driver.findElement(By.id("draggable2"));
+		WebElement drag3 = driver.findElement(By.id("draggable3"));
+		
+		action.clickAndHold(drag1).moveByOffset(0, 70).build().perform();
+		action.clickAndHold(drag2).moveByOffset(0, 100).build().perform();
+		action.clickAndHold(drag3).moveByOffset(0, 110).build().perform();
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,120)", "");
 	}
 
 }
