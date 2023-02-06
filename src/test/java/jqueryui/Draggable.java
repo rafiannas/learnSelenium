21776b00af69d560ad318b2609c71fd78d704abe
rafi <rafiannas.id@gmail.com>
2023-02-06 16:20:40 +0700

    first commit

---
+++ src/test/java/jqueryui/Draggable.java
@@ -0,0 +1,19 @@
+package jqueryui;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Draggable {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://jqueryui.com/draggable/");
+		Thread.sleep(5000);
+
+	}
+
+}
