c8a523e8b2b0de7e6c730aace1ef5ec8221d3f27
rafi <rafiannas.id@gmail.com>
2023-02-06 16:40:10 +0700

    adding default functionality

--- src/test/java/jqueryui/Draggable.java
+++ src/test/java/jqueryui/Draggable.java
@@ -1,7 +1,10 @@
 package jqueryui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,7 +15,15 @@ public class Draggable {
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://jqueryui.com/draggable/");
+		driver.manage().window().maximize();
 		Thread.sleep(5000);
+		
+//		default functionality
+		Actions action = new Actions(driver);
+		driver.switchTo().frame(0);
+		
+		WebElement drag = driver.findElement(By.id("draggable"));
+		action.clickAndHold(drag).moveByOffset(50, 50).build().perform();
 
 	}
 
